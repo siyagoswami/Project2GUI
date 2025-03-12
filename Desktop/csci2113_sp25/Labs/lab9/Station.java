f88a3ff121b64a86a2ffc6c95fc041331b62a2dd
Aurora Stankow-Mercer <aurora@Auroras-MacBook-Air.local>
2025-03-12 14:15:53 -0500

    Created Station object

--- Desktop/csci2113_sp25/Labs/lab9/Station.java
+++ Desktop/csci2113_sp25/Labs/lab9/Station.java
@@ -1,3 +1,15 @@
-public class Station {
+import java.util.*;
 
+public class Station {
+	
+	protected String name;
+	protected String lineColor;
+	protected boolean flag;
+	//protected ___ connection;
+	
+	public Station(String lineColor, String name) {
+		this.name = name;
+		this.lineColor = lineColor;
+		this.flag = true;
+	}
 }
